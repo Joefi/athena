d6bdef5d849342c3f2105ed5036ed7161be4d24c
laine <977579545@qq.com>
2020-04-04 20:05:56 +0800

    update

--- examples/asr/thchs30/local/prepare_vocab.py
+++ examples/asr/thchs30/local/prepare_vocab.py
@@ -1,4 +1,4 @@
-import os
+import sys
 from absl import logging
 import pandas
 import codecs
@@ -13,15 +13,15 @@ if __name__ == "__main__":
               '    vocab_file  : vocab file'.format(sys.argv[0]))
         exit(1)
 
-    train_csv = argv[1]
-    vocab_file = argv[2]
+    train_csv = sys.argv[1]
+    vocab_file = sys.argv[2]
     char_dict = {}
     char_dict['<unk>'] = 0
     char_dict['<space>'] = 0
-	texts = pd.read_csv(train_csv, sep='\t', usecols=['transcript'])
-	for text in texts.itertuples():
-		items = text.strip().split(" ")
-		for item in items[1:]:
+    texts = pandas.read_csv(train_csv, sep='\t', usecols=['transcript'])
+    for text in texts.itertuples():
+        items = text.strip().split(" ")
+        for item in items[1:]:
             if item in char_dict:
                 char_dict[item] += 1
             else:
@@ -29,10 +29,10 @@ if __name__ == "__main__":
     
     idx = 0
     with codecs.open(vocab_file, "r", "utf-8") as f
-    	for key in char_dict.keys():
-    		char_id_str = key + ' ' + str(idx) + '\n'
-    		f.write(char_id_str)
-    		idx += 1
+        for key in char_dict.keys():
+            char_id_str = key + ' ' + str(idx) + '\n'
+            f.write(char_id_str)
+            idx += 1
 
 
 
